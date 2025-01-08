5e126d8d2b7e13ba7b3493912851a08ac035a919
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-01-08 13:08:29 -0500

    mobile tweak

--- src/components/Options.tsx
+++ src/components/Options.tsx
@@ -15,7 +15,7 @@ export default function Options({
   handleSelected,
 }: Props) {
   return (
-    <div className="flex flex-col items-start gap-y-2">
+    <div className="flex flex-col items-center md:item-start gap-y-2">
       {/*the width is full*/}
       <h2 className="font-extrabold">{title.toUpperCase()}</h2>
       <div className="flex flex-wrap gap-2 max-w-[450px] p-4 border-red-700 rounded-lg">
